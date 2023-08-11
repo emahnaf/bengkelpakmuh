1718a5350f87b4871f279b5893d28c918751afcc
anaff <emahnaf@gmail.com>
2023-08-11 08:50:39 +0800

    ini commit sejak ke malang

--- grails-app/controllers/pelanggan.groovy
+++
@@ -1,5 +0,0 @@
-package bengkelpakmuh
-
-class pelanggan{
-
-}
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,14 @@
+import bengkelpakmuh.Pelanggan
+
 class BootStrap {
 
     def init = { servletContext ->
+//        new Pelanggan()
+//                .addToCategories(new Pelanggan(name: "DSLR"))
+//                .addToCategories(new ProductCategory(name: "Mirrorless"))
+//                .save()
+//
+
     }
     def destroy = {
     }
